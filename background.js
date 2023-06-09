bd023b56b33cd05d45ba57f3433dd3103bed31ec
Penguin <90953888+hxxwxx@users.noreply.github.com>
2023-06-10 03:49:26 +0900

    change image file road

---
+++ background.js
@@ -0,0 +1,6 @@
+const images = ['0.jpg','1.jpg','2.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','22.jpg','23.jpg'];
+const todaysImage=images[Math.floor(Math.random()*images.length)];
+const container = document.querySelector("#container");
+const image = document.createElement('img');
+image.src=`./images/${todaysImage}`;
+container.appendChild(image);
